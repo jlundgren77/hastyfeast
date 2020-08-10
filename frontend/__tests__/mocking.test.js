541ca35cd20c0f1c98b6adedf0b74ab6e08cbaa2
jlundgren77 <jlundgren77@gmail.com>
2020-08-10 11:52:24 -0400

    .gitignore working

--- frontend/__tests__/mocking.test.js
+++
@@ -1,8 +0,0 @@
-describe('mocking learning', () => {
-  it('mocks a function', () => {
-    const fetchDogs = jest.fn();
-    fetchDogs('snickers');
-    expect(fetchDogs).toHaveBeenCalled();
-    expect(fetchDogs).toHaveBeenCalledWith('snickers');
-  });
-});
